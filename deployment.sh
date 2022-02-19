1cac8ff5efff1b28e3d18670f55d13f2b1340252
Samuel Börlin <11892708+SamuelBoerlin@users.noreply.github.com>
2022-02-19 15:09:15 +0100

    no message

--- deployment.sh
+++ deployment.sh
@@ -27,43 +27,73 @@ finished=0
 connected=0
 errors_file="$(mktemp)"
 
+PSK_KEY_FILE="${PSK_KEY_FILE:-psk.key}"
+
+check_psk_file() {
+    [ ! -f "${PSK_KEY_FILE}" ] && echo "Could not find pre-shared key file '${PSK_KEY_FILE}'" >&2 && exit 1
+}
+
+read_identity() {
+    check_psk_file
+    cat "${PSK_KEY_FILE}" | sed 's/:.*//'
+}
+
+read_key() {
+    check_psk_file
+    cat "${PSK_KEY_FILE}" | sed 's/.*://g' | tr -d '\n' | xxd -p
+}
+
+[ -z "$(read_identity)" ] && echo "Failed reading pre-shared key identity" >&2 && exit 1
+[ -z "$(read_key)" ] && echo "Failed reading pre-shared key" >&2 && exit 1
+
+# Enable extended glob patterns for parsing
+shopt -s extglob
+
+connect() {
+    identity=$1
+    psk=$2
+    openssl s_client -tls1_2 -quiet -verify_quiet -psk_identity $identity -psk $psk -connect ${host}:${port} 2>"$errors_file"
+}
+
 for i in $(seq 1 $retries); do
     if [ "$finished" -ne 0 ] || [ "$connected" -ne 0 ]; then break; fi
 
-    # Try opening connection and send command
-    if exec 2>"$errors_file" 3<>/dev/tcp/${host}/${port} && echo "$@" 1>&3 ; then
-        # Read response
-        while [ "$finished" -eq 0 ] && IFS= read -r x <&3 2>"$errors_file"; do
-            if [ ! -z "$x" ]; then
-                case "$x" in
-                    "EOF") finished=1 ;;
-                    "STATUS: "*) status=${x#????????} ;;
-                    *) if [ "$i" -ne 1 ] && [ "$connected" -ne 1 ]; then echo ""; fi; echo "$x" ;;
-                esac
+    # Try opening connection, send command and read response
+    while IFS= read -r x 2>"$errors_file"; do
+        case "$x" in
+            "${COMMAND_RESPONSE_STATUS:-STATUS}: "@([-]+([0-9])|+([0-9])))
+                status=${x#????????}
+                [ -z "$COMMAND_RESPONSE_EOF" ] && finished=1
+                ;;
+            *)
+                if [ ! -z "$COMMAND_RESPONSE_EOF"] && [ "$x" = "$COMMAND_RESPONSE_EOF" ]; then
+                    finished=1
+                else
+                    if [ "$i" -ne 1 ] && [ "$connected" -ne 1 ]; then
+                        echo ""
+                    fi
+                    echo "$x"
+                fi
+                ;;
+        esac
 
-                connected=1
-            else
-                break
-            fi
-        done
+        connected=1
 
-        # Response not yet ready
-        if [ "$finished" -eq 0 ]; then
-            echo -n "."
-            [ "$i" -ne "$retries" ] && sleep 1
-        fi
-    else
+        if [ "$finished" -ne 0 ]; then break; fi
+    done < <(echo "$@" | connect "$(read_identity)" $(read_key))
+
+    # Something went wrong, try again
+    if [ "$finished" -eq 0 ]; then
+        connected=0
         echo -n "."
         [ "$i" -ne "$retries" ] && sleep 1
     fi
+
 done
 
 # Print out errors
 if [ "$connected" -ne 1 ]; then echo ""; fi
-if [ "$finished" -eq 0 ]; then cat "$errors_file"; fi
-
-# Close connection
-exec 3<&- ; exec 3>&-
+if [ "$finished" -eq 0 ]; then cat "$errors_file" >&2; fi
 
 # Exit with status code in response
 exit $status
